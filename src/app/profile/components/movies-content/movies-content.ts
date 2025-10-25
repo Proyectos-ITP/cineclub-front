c653baf0d7dd4e25a87977ae1f4e6fea41a827d7
unknown <jhonlegarda1.2@gmail.com>
2025-10-25 17:05:45 -0500

    Added: json info card movies

--- src/app/profile/components/movies-content/movies-content.ts
+++ src/app/profile/components/movies-content/movies-content.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LibraryInterface } from '../../interfaces/library.interface';
+import { MOVIES } from '../../constants/library.constants';
+import { CardRecomendations } from '../card-recomendations/card-recomendations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movies-content',
   standalone: true,
-  imports: [],
+  imports: [CardRecomendations, CommonModule],
   templateUrl: './movies-content.html',
   styleUrl: './movies-content.scss',
 })
-export class MoviesContent {}
+export class MoviesContent {
+  movies: LibraryInterface[] = MOVIES;
+}
