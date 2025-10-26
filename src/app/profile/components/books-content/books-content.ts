a3ccd10dfd0bb63fc0ea175318bff7d6eee69a40
Jairo-18 <jhonlegarda1.2@gmail.com>
2025-10-25 20:01:25 -0500

    Stiliced card user

--- src/app/profile/components/books-content/books-content.ts
+++ src/app/profile/components/books-content/books-content.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LibraryInterface } from '../../interfaces/library.interface';
+import { CardRecomendations } from '../card-recomendations/card-recomendations';
+import { CommonModule } from '@angular/common';
+import { BOOKS } from '../../constants/library.constants';
 
 @Component({
   selector: 'app-books-content',
   standalone: true,
-  imports: [],
+  imports: [CardRecomendations, CommonModule],
   templateUrl: './books-content.html',
   styleUrl: './books-content.scss',
 })
-export class BooksContent {}
+export class BooksContent {
+  books: LibraryInterface[] = BOOKS;
+}
--- src/app/profile/pages/user-profile/user-profile.ts
+++ src/app/profile/pages/user-profile/user-profile.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardInfoUser } from '../../components/card-info-user/card-info-user';
 import { CardTabs } from '../../components/card-tabs/card-tabs';
+import { CardStatics } from '../../components/card-statics/card-statics';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule, CardInfoUser, CardTabs],
+  imports: [CommonModule, CardInfoUser, CardTabs, CardStatics],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.scss',
 })
