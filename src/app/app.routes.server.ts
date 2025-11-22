bafa63b512665eb54562a2152ff68c3fe78bfe31
Jheff <divegas43@gmail.com>
2025-11-22 17:39:54 -0500

    feat: precommit implement and adjust ssr routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'profile/friend-profile/:friendId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'organizational/edit-user/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
